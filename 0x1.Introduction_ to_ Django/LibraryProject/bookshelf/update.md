20daff503eb8d8cf6374dae6023c5706e90feac5
RUTAJOGA <mac@Owens.local>
2024-09-17 18:29:06 +0200

    Django files commit

---
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/update.md
@@ -0,0 +1,1 @@
+book = Book.objects.get(id=1) book.title= "Nineteen Eighty-Four" book.save()
---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,5 @@
+#SELECT * FROM books WHERE author = 'Example';
+#  
+Library.objects.get(name=library_name)", "books.all()
+Author.objects.get(name=author_name)", "objects.filter(author=author)
+"Librarian.objects.get(library="
